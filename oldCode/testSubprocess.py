6dfb9ee6b0bdd20591a6b70603b3ed56ad460cec
spencerpeters <spencer@goebelpeters.com>
2018-01-19 17:34:22 +0100

    Clean house.

--- oldCode/testSubprocess.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'Spencer'
-
-import subprocess
-
-for i in range(100):
-    subprocess.call(["Rscript",
-                     "/Users/Spencer/R-Projects/AdjustmentEnumeration/profilePythonSubprocess.R"])
